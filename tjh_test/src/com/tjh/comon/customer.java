ebb7690a44381a62832726280b6778fdb4d2b290
tangjunhua <2414932725@qq.com>
2017-08-30 09:58:16 +0800

    checkin this java  project

---
+++ tjh_test/src/com/tjh/comon/customer.java
@@ -0,0 +1,7 @@
+package com.tjh.comon;
+
+public class customer {
+    public static void main(String[] args) {
+		System.out.println("customer类！！！");
+	}
+}
---
+++ tjh_test/src/tjh/student.java
@@ -0,0 +1,7 @@
+package tjh;
+
+public class student {
+  public static void main(String[] args) {
+	  System.out.println("这是一个测试文件");
+}
+}
